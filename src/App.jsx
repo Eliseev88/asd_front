3694f9ab7c1484fbfdab7d2a569f86be59c5df29
Eliseev88 <eliseev88@gmail.com>
2025-11-05 17:32:46 +0300

    fix hash reload page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Suspense } from "react";
+import { Suspense, useEffect } from "react";
 import Content from "./components/Content";
 import Header from "./components/Header";
 import "./styles/App.scss";
@@ -8,6 +8,24 @@ import { CollapseProvider } from "./providers/CollapseProvider";
 import { TariffProvider } from "./providers/TariffProvider";
 
 function App() {
+  const hash = location.hash;
+
+  useEffect(() => {
+    if (!hash) return;
+
+    const tryScroll = () => {
+      const el = document.querySelector(hash);
+
+      if (el) {
+        el.scrollIntoView({ behavior: "smooth", block: "start" });
+      } else {
+        setTimeout(tryScroll, 50);
+      }
+    };
+
+    tryScroll();
+  }, [hash]);
+
   return (
     <Suspense fallback="">
       <CollapseProvider>
